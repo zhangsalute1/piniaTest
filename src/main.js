3bd276fb0b62e513f470c5c5f737c6aaf991b430
zhangJingJie <zhangsalute1@gmail.com>
2024-01-31 19:30:58 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -7,5 +7,11 @@ import { createPinia } from 'pinia'
 //创建pinia
 const pinia = createPinia()
 
+//测试接口函数
+import { getCategoryAPI } from '@/apis/testAPI.js'
+getCategoryAPI().then(res => {
+    console.log(res)
+})
+
 //创建vue实例
 createApp(App).use(pinia).mount('#app')
