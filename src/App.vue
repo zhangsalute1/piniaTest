568cf9305aa1d83a874ccf20258dc2f8aa56c940
zhangJingJie <zhangsalute1@gmail.com>
2024-01-31 20:13:19 +0800

    一级路由创建完毕

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <el-button type="primary">Primary</el-button>
   <!-- 一级路由出口组件 -->
+  <router-view></router-view>
 </template>
 
 
--- src/main.js
+++ src/main.js
@@ -7,10 +7,29 @@ import { createPinia } from 'pinia'
 //创建pinia
 const pinia = createPinia()
 
-//测试接口函数
-import { getCategoryAPI } from '@/apis/testAPI.js'
-getCategoryAPI().then(res => {
-    console.log(res)
+// //测试接口函数
+// import { getCategoryAPI } from '@/apis/testAPI.js'
+// getCategoryAPI().then(res => {
+//     console.log(res)
+// })
+
+import { createRouter, createWebHistory } from 'vue-router'
+import login from "@/components/login.vue"
+import layout from "@/components/layout.vue"
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: layout
+        },
+        {
+            path: '/login',
+            component: login
+        }
+    ]
 })
+
+
 //创建vue实例
-createApp(App).use(pinia).mount('#app')
+createApp(App).use(pinia).use(router).mount('#app')
