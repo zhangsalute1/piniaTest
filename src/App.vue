d8ac223bec987f94c785afd2478eff16b8ceb6f5
zhangJingJie <zhangsalute1@gmail.com>
2024-01-31 18:47:07 +0800

    引入elementbutton组件

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
 <template>
-  <button @click="counterStore.increment">{{ count }}</button>
-  {{ doubleCount }}
-  <ul>
-    <li v-for="item in counterStore.list" :key="item.id">{{ item.name }}</li>
-  </ul>
+  <el-button type="primary">Primary</el-button>
 </template>
 
 
 <script setup>
-import { useCounterStore } from '../src/stores/counter'
-import { onMounted } from 'vue'
-import { storeToRefs } from 'pinia'
-
-const counterStore = useCounterStore()
-const { count, doubleCount, } = storeToRefs(counterStore)
-
-onMounted(() => {
-  counterStore.getList()
-  console.log(1);
-})
 
 </script>
 <style></style>
