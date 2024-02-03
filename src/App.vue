21341fc5d3cf7d5ecaeac155f3841d8f5763687f
zhangJingJie <zhangsalute1@gmail.com>
2024-02-03 22:13:17 +0800

    配置scss自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <!-- 一级路由出口组件 -->
   <router-view></router-view>
+  <div class="test">
+    Test scss
+  </div>
 </template>
 
 
@@ -8,4 +11,8 @@
 
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
+</style> 
