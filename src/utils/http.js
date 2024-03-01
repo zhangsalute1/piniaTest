55620e7c1ac4994e1cb7ec9d88e780166d41ef8d
zhangJingJie <zhangsalute1@gmail.com>
2024-03-02 00:36:41 +0800

    登录里面的token拦截器携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,8 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from '@/stores/user'
+
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout: 5000,
@@ -9,6 +11,12 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+    //从pinia里面获取数据
+    const userStore = useUserStore()
+    const token = userStore.userInfo.result.token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, e => Promise.reject(e))
 
